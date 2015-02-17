8b5e7df2b83413494a87ed67c9b9e045dad2cd2a
redpois0n <git@redpois0n>
2015-02-17 11:22:12 +0100

    Checks if modulo 512 is 0

--- src/tchide/Main.java
+++ src/tchide/Main.java
@@ -1,9 +1,19 @@
 package tchide;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		try {
+			File file = new File(args[0]);
+			
+			boolean modulo = file.length() % 512 == 0;
+
+			System.out.println("Modulo: " + modulo);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
